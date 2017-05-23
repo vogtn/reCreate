915d382700f76276a3c11c338fea8a41860640e3
vogtn <nicolasjvogt@gmail.com>
2017-05-22 21:39:07 -0700

    helpers for clicking actions

--- src/SideBar.js
+++ src/SideBar.js
@@ -7,13 +7,25 @@ class SideBar extends Component {
         return(
             <div className="sidebar">
                 <ul>
-                    <li><h1>H1 Text</h1></li>
-                    <li><h2>H2 Text</h2></li>
-                    <li><button>Button</button></li>
-                    <li><input readOnly type="text"></input></li>
+                    <li><h1 onClick={() =>this._h1Click()}>H1 Text</h1></li>
+                    <li><h2 onClick={() => this._h2Click()}>H2 Text</h2></li>
+                    <li><button onClick={() => this._buttonClick()}>Button</button></li>
+                    <li><input readOnly type="text" onClick={() => this._inputClick()}></input></li>
                 </ul>
             </div>
         )
+    }
+    _h1Click = () => {
+        console.log('h1 was clicked');
+    }
+    _h2Click = () => {
+        console.log('h2 was clicked');
+    }
+    _buttonClick = () =>{
+        console.log('Button was clicked');
+    }
+    _inputClick = () => {
+        console.log('input was clicked');
     }
 }
 
