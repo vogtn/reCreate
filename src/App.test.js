e606b3da4118af0b100e79d0ca33f4e1e5f3d02d
vogtn <nicolasjvogt@gmail.com>
2017-05-25 13:37:44 -0700

    basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+describe('App', () => {
+    it('should be able to run tests', () => {
+        expect(1 + 2).toEqual(3);
+    });
+});
